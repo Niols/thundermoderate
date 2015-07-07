6a216203db6ca38893cb4a20155c1e22f00ff78d
Nicolas Jeannerod <nicolas.jeannerod@ens.fr>
2015-07-07 17:53:36 -0400

    add parameter for showing moderator or not

--- chrome/content/Settings.js
+++ chrome/content/Settings.js
@@ -10,12 +10,18 @@ var Settings = {
     {
 	var token = document.getElementById('thundermoderate-settings-token').value;
 	nsPreferences.setUnicharPref('thundermoderate.token', token);
+
+	var showModeratorInButton = document.getElementById('thundermoderate-settings-moderator-showinbutton').checked;
+	nsPreferences.setBoolPref('thundermoderate.moderator.showinbutton', showModeratorInButton);
     },
 
     onLoad: function()
     {
 	var tokenInput = document.getElementById('thundermoderate-settings-token');
 	tokenInput.value = nsPreferences.copyUnicharPref('thundermoderate.token', '');
+
+	var showModeratorInButtonInput = document.getElementById('thundermoderate-settings-moderator-showinbutton');
+	showModeratorInButtonInput.checked = nsPreferences.getBoolPref('thundermoderate.moderator.showinbutton', false);
     }
 
 }
